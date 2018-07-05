8483dc19e996c71f0e6b0edc19472816ce119b5e
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-07-06 00:00:04 +0200

    ReadingUserInputChallenge

--- src/readingUserInputChallenge/Main.java
+++ src/readingUserInputChallenge/Main.java
@@ -1,9 +1,10 @@
+package readingUserInputChallenge;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int counter = 0;
         int sum = 0;
 
