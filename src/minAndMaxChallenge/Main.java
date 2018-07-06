807b16b31951dfa56798cc3b198cae4f00cc84a3
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-07-06 19:00:20 +0200

    minAndMaxChallenge

--- src/minAndMaxChallenge/Main.java
+++ src/minAndMaxChallenge/Main.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
 
-        int min = 0;
-        int max = 0;
-        boolean first = true;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+      //  boolean first = true;
 
         while (true) {
             System.out.println("Enter number: ");
@@ -17,11 +17,11 @@ public class Main {
 
             if (isAnInt) {
                 int number = scanner.nextInt();
-                if(first){
-                    first = false;
-                    min = number;
-                    max = number;
-                }
+//                if(first){
+//                    first = false;
+//                    min = number;
+//                    max = number;
+//                }
 
                 if (number > max) {
                     max = number;
