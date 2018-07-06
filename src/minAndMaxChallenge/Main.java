3e29fa08cb99305b2f28b8c89fbc7795e21c3fe7
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-07-06 18:08:43 +0200

    minAndMaxChallenge

---
+++ src/minAndMaxChallenge/Main.java
@@ -0,0 +1,27 @@
+package minAndMaxChallenge;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int min = 0;
+        int max = 0;
+
+        while(true){
+            System.out.println("Enter number: ");
+            boolean isAnInt = scanner.hasNextInt();
+
+            if(isAnInt){
+
+            }else{
+                
+            }
+
+
+        }
+
+        scanner.close();
+    }
+}
